023a01661c070ff726f14b5fef532058a1648184
Ian Shellman <ianshellman@gmail.com>
2013-07-20 16:33:02 -0400

    update

--- Back to Basics/src/backTobasics/GameClass.java
+++ Back to Basics/src/backTobasics/GameClass.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 
 public class GameClass {
 
-	private Scanner in;
+	private Scanner scanner;
 	
 	public GameClass ()	{
-		in = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 	}
 	
 	public boolean PromptToPlay (boolean i) 	{
 		System.out.println("Do you want to play the number game? (enter Y for yes, or anyhting else for no)");
-		String user_input = in.nextLine();
+		String user_input = scanner.nextLine();
 		if (user_input.equalsIgnoreCase("Y"))	{
 			return true;
 		}
 		return false;
 	}
 
+	public void play() {
+		System.out.println("Think of a number between 0 and 100");
+		System.out.println("Are you ready?");
+		
+	}
 }
