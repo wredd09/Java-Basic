8385c48959cc6d6a64d1accfa913b6dac790bf6d
Ian Shellman <ianshellman@gmail.com>
2013-07-20 16:12:39 -0400

    setup commit

--- Back to Basics/src/backTobasics/HelloWorld.java
+++ Back to Basics/src/backTobasics/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		GameClass a = GameClass;
+		
 		for (int i = 0; i <= 7; i++ ){
 			System.out.println("Hello World");
 			System.out.println("boom shaka lakka");
