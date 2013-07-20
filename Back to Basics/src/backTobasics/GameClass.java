942ec837c339dbc3fa305349d79460a882ea7f5f
WilliamRedd <w.redd09@gmail.com>
2013-07-20 15:01:47 -0500

    Working on it

---
+++ Back to Basics/src/backTobasics/GameClass.java
@@ -0,0 +1,17 @@
+package backTobasics;
+
+public class GameClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int low, high, guesses;
+		System.out.println("This is the game.");
+		System.out.println("You just lost");
+
+	}
+	
+	public static PromptToPlay {
+		
+	}
+
+}
