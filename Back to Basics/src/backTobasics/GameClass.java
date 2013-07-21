e73b629d58fa300981572e5aee4e06059b3a9aa9
WilliamRedd <w.redd09@gmail.com>
2013-07-20 20:01:26 -0500

    blah

--- Back to Basics/src/backTobasics/GameClass.java
+++ Back to Basics/src/backTobasics/GameClass.java
@@ -2,40 +2,42 @@ package backTobasics;
 
 import java.util.Random;
 import java.util.Scanner;
-
-public class GameClass {
-
-	private Scanner scanner;
-	private Random random;
+public static void main(String[] args) {
 	
-	public GameClass ()	{
-		scanner = new Scanner(System.in);
-		random = new Random();
-		play();
-	}
-	
-	public boolean PromptToPlay () 	{
-		System.out.println("Do you want to play the number game? "
-				+ "(enter Y for yes, or anyhting else for no)");
-		String user_input = scanner.nextLine();
-		if (user_input.equalsIgnoreCase("Y"))	{
-			return true;
-		}
-		return false;
-	}
+	public class GameClass {
 
-	public void play() {
-		int low, high, guesses;
-		System.out.println("What do you want set as your low number?");
-		low = scanner.nextInt(); 
-		scanner.nextLine();
-		System.out.println("What do you want to set as your high number?");
-		high = scanner.nextInt();
-		scanner.nextLine();
-		System.out.println();
-		int n = high - low;
-		int randomnumber = random.nextInt();
+		private Scanner scanner;
+		private Random random;
 		
+		public GameClass ()	{
+			scanner = new Scanner(System.in);
+			random = new Random();
+			play();
+		}
 	
+		public boolean PromptToPlay () 	{
+			System.out.println("Do you want to play the number game? "
+					+ "(enter Y for yes, or anyhting else for no)");
+			String user_input = scanner.nextLine();
+			if (user_input.equalsIgnoreCase("Y"))	{
+				return true;
+			}
+			return false;
+		}
+		
+		public void play() {
+			int low, high, guesses;
+			System.out.println("What do you want set as your low number?");
+			low = scanner.nextInt(); 
+			scanner.nextLine();
+			System.out.println("What do you want to set as your high number?");
+			high = scanner.nextInt();
+			scanner.nextLine();
+			System.out.println();
+			int n = high - low;
+			int randomnumber = random.nextInt() % n;
+			System.out.println(randomnumber);
+		
+		}
 	}
 }
