c84284457f3d2defaf095209d23f76cbf33ac2c4
Will Redd <w.redd09@gmail.com>
2013-07-22 15:51:36 -0500

    More update to the game class

--- Back to Basics/src/backTobasics/GameClass.java
+++ Back to Basics/src/backTobasics/GameClass.java
@@ -4,79 +4,148 @@ import java.util.*;
 
 public class GameClass {
 
-	public static void main(String[] args) {
-		Scanner scanner;
-		Random random;
-		scanner = new Scanner(System.in);
-		random = new Random();
-		if (PromptToPlay(scanner)) {
-			play(scanner, random);
-		} else {
-			System.out.println("Goodbye");
-		}
+	private static final Scanner scanner = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		if (promptToPlay()) {
+			play();
+			while (playAgain()) {
+				play();
+			}
+		}
+		System.out.println("Goodbye");	
 	}
 
-	public static boolean PromptToPlay(Scanner scanner) {
-		System.out.println("Do you want to play the number game? "
-				+ "(enter Y for yes, or anything else for no)");
+	public static boolean promptToPlay() {
+		System.out.println("Do you want to play the number game?");
 		String user_input = scanner.nextLine();
-		if (user_input.equalsIgnoreCase("Y")) {
+		if (Character.toUpperCase(user_input.charAt(0))== 'Y') {
 			return true;
 		}
 		return false;
 	}
-
-	public static void play(Scanner scanner, Random random) {
-		int randomnumber = numbergenerator(scanner, random);
-		// System.out.println("random number is " + randomnumber);
-		int guesses = actualgame(randomnumber, scanner);
-		System.out.println("It took you " + guesses + " Guesses");
+	
+	public static boolean playAgain() {
+		System.out.println("Would you like to play again?");
+		String user_input = scanner.nextLine();
+		if (Character.toUpperCase(user_input.charAt(0))== 'Y') {
+			System.out.println("");
+			return true;
+		}
+		return false;
+	}
+	
+	public static int getInt() {
+		int userInt = 0;
+		boolean successful = true;
+		do {
+			try {
+				userInt = Integer.parseInt(scanner.nextLine());
+				successful = true;
+			} catch (Exception e) {
+				successful = false;
+				System.out.println("That is not valid. Please enter an integer.");
+			}
+		} while (!successful);
+		return userInt;
 	}
 
-	public static int numbergenerator(Scanner scanner, Random random) {
-		int low, high, randomnumber;
+	public static void play() {
+		Random random = new Random();
+		int [] randlowhigh = numbergenerator(random);
+		// next line is debug only
+		System.out.println("rand low high " + randlowhigh[0] + " " + randlowhigh [1] 
+				+ " " + randlowhigh [2]);
+		int guesses = actualgame(randlowhigh);
+		if (guesses > 1) {
+			System.out.println("It took you " + guesses + " guesses.");
+		}
+	}
+
+	public static int [] numbergenerator(Random random) {
+		int low = 0, high = 0;
+		int [] randlowhigh = new int [3];
 		System.out.println("What do you want set as your low number?");
-		low = scanner.nextInt();
-		// scanner.nextLine();
+		low = getInt();
 		System.out.println("What do you want to set as your high number?");
-		high = scanner.nextInt();
-		// scanner.nextLine();
-		System.out.println("low is " + low + ". High is " + high);
-		randomnumber = random.nextInt(high - low + 1); // WHOOOOO Comments
+		do {
+			high = getInt();
+			if (high <= low){
+				System.out.println("Please enter a number larger than your low value");
+			}
+		} while (high <= low);
+		
+		int randomnumber = random.nextInt(high - low + 1); // WHOOOOO Comments
 		randomnumber = randomnumber + low;
-		return randomnumber;
+		randlowhigh[0] = randomnumber;
+		randlowhigh[1] = low;
+		randlowhigh[2] = high;
+		return randlowhigh ;
 	}
 
-	public static int actualgame(int randomnumber, Scanner scanner) {
-		int guess = 0;
-		int guesses = 0;
+	public static int actualgame(int[] randlowhigh) {
+		int guess = 0, guesses = 0;
+		int [] currentlohi = new int [2];
+		currentlohi[0] = randlowhigh[1];
+		currentlohi[1] = randlowhigh[2];
 		System.out.print("Guess a number ");
-		guess = scanner.nextInt();
+		guess = getInt();
 		System.out.println();
-		if (guess == randomnumber) {
+		if (guess == randlowhigh[0]) {
 			guesses++;
 			System.out.println("Lucky Guess!");
+			System.out.println();
 			return guesses;
 		} else {
 			do {
-				guesses++;
-				if (guess < randomnumber) {
-					System.out.print("Too low. Guess again ");
-					guess = scanner.nextInt();
-					System.out.println();
-				} else if (guess > randomnumber) {
-					System.out.print("Too high. Guess again ");
-					guess = scanner.nextInt();
-					System.out.println();
+				if (guess >= currentlohi[0] && guess <= currentlohi[1]){
+					if (guess < randlowhigh[0]) {
+						System.out.print("Too low. Guess again ");
+						currentlohi[0] = guess;
+						System.out.println();
+					} else if (guess > randlowhigh[0]) {
+						System.out.print("Too high. Guess again ");
+						currentlohi[1] = guess;
+						System.out.println();
+					}
+					guesses++;
+					
+				} else if (guess < randlowhigh[1] || guess > randlowhigh[2]){
+					System.out.println("Guess was out of original range. Guess again.");
+				} else if (guess <= currentlohi[0] && currentlohi[0] != randlowhigh [1]){
+					System.out.println("I've already told you the number is greater than " 
+							+ currentlohi[0]);
+					if (guess == currentlohi[0]){
+						currentlohi[0] = guess;
+					}
+				} else if (guess >= currentlohi[1] && currentlohi[1] != randlowhigh [2]){
+					System.out.println("I've already told you the number is less than " 
+							+ currentlohi[1]);
+					if (guess == currentlohi[1]) {
+						currentlohi[1] = guess;
+					}
 				}
-			} while (guess != randomnumber);
+				
+				guess = getInt();
+				
+			} while (guess != randlowhigh[0]);
 		}
 
-		if (guess == randomnumber) {
+		if (guess == randlowhigh[0]) {
 			guesses++;
 			System.out.println("Got it!");
 		}
 		return guesses;
+	}
+	
+	public static boolean isValid(int guess, int[] randlowhigh, int[] currenthilo){
+		if (guess < randlowhigh[1] || guess > randlowhigh[2]) {
+			do {
+				System.out.println("Guess was out of range. Guess Again");
+				guess = getInt();
+			} while (guess < randlowhigh[1] || guess > randlowhigh[2]);
+		}
+		return false;
+		
 	}
 }
--- Back to Basics/src/backTobasics/HelloWorld.java
+++ Back to Basics/src/backTobasics/HelloWorld.java
@@ -5,11 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		GameClass gc = new GameClass();
-
+/*
 		while (gc.PromptToPlay()) {
 			gc.play();
 		}
-
+*/
 		System.exit(0);
 	}
 }
